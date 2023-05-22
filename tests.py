84fb615af66263edf89a976effff09fcfd79f3d9
Maria Starinova <starinovamaria@yandex.ru>
2023-05-22 10:25:45 +0300

    Sprint_2_corrected

--- tests.py
+++ tests.py
@@ -2,79 +2,54 @@ import pytest
 
 from main import BooksCollector
 
+name = 'Что делать, если ваш кот хочет вас убить'
+other_name = 'Синдром петрушки'
+
+
 class TestBooksCollector:
-
-    @pytest.fixture(scope='function')
-    def books_collector(self):
-        return BooksCollector()
-
-    name = 'Что делать, если ваш кот хочет вас убить'
-    other_name = 'Синдром петрушки'
-
     def test_add_new_book_and_default_rating(self, books_collector):
-        books_collector.add_new_book(self.name)
+        books_collector.add_new_book(name)
 
-        assert books_collector.favorites == []
-        assert books_collector.books_rating == {self.name: 1}
-
+        assert books_collector.get_book_rating(name) == 1
 
     def test_add_new_book_not_add_twice(self, books_collector):
-        books_collector.add_new_book(self.name)
-        books_collector.add_new_book(self.name)
+        books_collector.add_new_book(name)
+        books_collector.add_new_book(name)
 
-        assert books_collector.favorites == []
-        assert books_collector.books_rating == {self.name: 1}
-
-
-    def test_add_book_in_favorites(self, books_collector):
-        books_collector.add_new_book(self.name)
-        books_collector.add_book_in_favorites(self.name)
-
-        assert self.name in books_collector.favorites
-
+        assert len(books_collector.get_books_rating()) == 1, 'Error:not add book twice'
 
     def test_get_books_rating_book_not_list_not_rating(self, books_collector):
         name = 'Гарри Поттер и философский камень'
-        rating = 10
 
-        assert books_collector.books_rating.get(name) is None
-
+        assert books_collector.get_book_rating(name) is None
 
     def test_get_books_rating_not_add_less_one(self, books_collector):
-        books_collector.add_new_book(self.name)
-        books_collector.set_book_rating(self.name, 0)
+        books_collector.add_new_book(name)
+        books_collector.set_book_rating(name, 0)
 
-        assert books_collector.books_rating[self.name] == 1
-
+        assert books_collector.get_book_rating(name) == 1
 
     def test_get_books_rating_not_add_more_ten(self, books_collector):
-        books_collector.add_new_book(self.name)
-        books_collector.set_book_rating(self.name, 15)
+        books_collector.add_new_book(name)
+        books_collector.set_book_rating(name, 15)
 
-        assert books_collector.books_rating[self.name] == 1
-
+        assert books_collector.get_book_rating(name) == 1
 
     def test_get_books_rating_not_add_book_no_rating(self, books_collector):
-        books_collector.add_new_book(self.name)
-        rating = books_collector.get_book_rating(self.other_name)
+        books_collector.add_new_book(name)
+        books_collector.set_book_rating(other_name, 3)
 
-        assert rating is None
-
+        assert books_collector.get_book_rating(name) == 1
 
     def test_add_book_in_favorites(self, books_collector):
-        books_collector.add_book_in_favorites(self.name)
+        books_collector.add_new_book(name)
+        books_collector.add_book_in_favorites(name)
 
-        assert books_collector.favorites == []
-        assert books_collector.books_rating == {}
-
+        assert name in books_collector.get_list_of_favorites_books()
 
     def test_delete_book_from_favorite(self, books_collector):
-        books_collector.add_new_book(self.name)
-        books_collector.add_book_in_favorites(self.name)
-        books_collector.delete_book_from_favorites(self.name)
+        books_collector.add_new_book(name)
+        books_collector.add_book_in_favorites(name)
+        books_collector.delete_book_from_favorites(name)
 
-        assert books_collector.favorites == []
-        assert books_collector.books_rating == {self.name: 1}
-
-
-
+        assert name not in books_collector.get_list_of_favorites_books()
